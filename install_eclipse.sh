4c8d03657f9f34c37d99b9198db02c69e6eba420
Anissa Kheireddine <ani.kheireddine@hotmail.com>
2017-06-30 15:59:20 +0200

    modif travis, install_eclipse.sh

--- install_eclipse.sh
+++ install_eclipse.sh
@@ -3,7 +3,7 @@
 set -x
 
 if [ ! -f eclipse/eclipse ] ; then 
-  wget https://yanntm.github.io/ITS-commandline/itscl_linux.tgz
+  wget https://anissakh.github.io/ITS-commandline/itscl_linux.tgz
   tar xzf itscl_linux.tgz
   rm itscl_linux.tgz
 fi
